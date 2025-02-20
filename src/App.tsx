8af2938ff6ba1a76a3a11bf2dc8ef308812d44bb
Hamza khan <hamzakhan127109@gmail.com>
2025-02-20 19:37:59 +0500

    build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { Button } from "@chakra-ui/react"
+import { Grid, GridItem } from "@chakra-ui/react"
 
 
 function App() {
-  return (<>
-    <Button>Click me</Button>
-    <p>hamza khan</p>
-    </>
+  return (<Grid templateAreas={{
+    base: `"nav" "main"`,
+    lg: `"nav nav""aside main"`
+  }}>
+    <GridItem area='nav' bg='coral'>Nav</GridItem>
+    <GridItem area='aside' bg='gold'>Aside</GridItem>
+    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+  </Grid>
   )
-  
+
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 import { system } from "@chakra-ui/react/preset"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ChakraProvider value={system}>
-        <App />
+      <App />
     </ChakraProvider>
   </StrictMode>,
 )
