fbb34a6ef3ea28e085b1e4f31d200e2e089bff56
Hamza khan <hamzakhan127109@gmail.com>
2025-03-13 15:51:44 +0500

    CleanUp the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
     return (
         <>
             {error && <p>{error}</p>}
-            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} padding='10px' gap={3}>
+            <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding='10px' gap={6}>
                 {data.map(game =>
                     <GameCard key={game.id} game={game} />
                 )}
