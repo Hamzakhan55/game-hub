c3892032061ffac54672de2219b41aa0940ca875
Hamza khan <hamzakhan127109@gmail.com>
2025-03-28 03:04:30 +0500

    Fetch Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,42 +8,47 @@ interface Props {
 }
 
 const GenreList = ({ selectedGenre, onSelectedGenre }: Props) => {
-
     const { data } = useGenres();
-
-
 
     return (
         <>
-            <Heading fontSize='2xl' marginBottom={3} style={{ color: 'white' }}>Genres</Heading>
-            <List.Root>
-                {data.map(genre => <ListItem key={genre.id} paddingY='5px' listStyle='none' >
-                    <HStack>
-                        <Image boxSize='32px' borderRadius={8} objectFit='cover' src={getCroppedImageUrl(genre.image_background)} />
-                        <Button
-                            style={{ color: 'white' }}
-                            width="80%"
-                            minHeight="40px"
-                            whiteSpace="normal"
-                            textAlign="center"
-                            fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-                            onClick={() => onSelectedGenre(genre)}
-                            fontSize="lg"
-                            variant="outline"
-                            padding="12px"
-                            margin="0"
-                            borderRadius="8px"
-                            _hover={{ color: 'white', bg: 'gray.700' }}
-                            _focus={{ color: 'white', bg: 'gray.700' }}
-                        >
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>)
-                }
-            </List.Root >
+            <Heading fontSize="2xl" marginBottom={3} color="white">
+                Genres
+            </Heading>
+            <List.Root gap={2}>
+                {data?.results.map((genre) => (
+                    <ListItem key={genre.id} paddingY="5px">
+                        <HStack>
+                            <Image
+                                boxSize="32px"
+                                borderRadius={8}
+                                objectFit="cover"
+                                src={getCroppedImageUrl(genre.image_background)}
+                            />
+                            <Button
+                                color="white"
+                                width="80%"
+                                minHeight="40px"
+                                whiteSpace="normal"
+                                textAlign="center"
+                                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                                onClick={() => onSelectedGenre(genre)}
+                                fontSize="lg"
+                                variant="outline"
+                                padding="12px"
+                                margin="0"
+                                borderRadius="8px"
+                                _hover={{ color: "white", bg: "gray.700" }}
+                                _focus={{ color: "white", bg: "gray.700" }}
+                            >
+                                {genre.name}
+                            </Button>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List.Root>
         </>
-    )
-}
+    );
+};
 
 export default GenreList;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,6 @@
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 
 export interface Genre {
     id: number;
@@ -6,6 +8,17 @@ export interface Genre {
     image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+interface FetchResponse<T> {
+    results: T[];
+}
+
+const useGenres = () =>
+    useQuery({
+        queryKey: ["genres"],
+        queryFn: () =>
+            apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+        staleTime: 24 * 60 * 60 * 1000, // Cache data for 24 hrs
+        initialData: { results: genres }
+    });
 
 export default useGenres;
